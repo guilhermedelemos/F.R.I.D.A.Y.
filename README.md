22286f5b87b111a46b03458ecada395daab68e70
Guilherme de Lemos <guilherme.eti@gmail.com>
2018-06-26 17:34:42 -0300

    Atualização da estação Ubuntu

--- README.md
+++ README.md
@@ -1,19 +1,19 @@
 ## De novo nao :cry:
 Documentação para reinstalação do meu sistema.
 
-Este repositório está separado em estações de trabalho Windows e Debian e servidor Debian para desenvolvimento.
+Este repositório está separado em estações de trabalho Windows, Debian e Ubuntu e servidor Debian para desenvolvimento.
 
-Infelizmente um [Mac](https://www.apple.com/br/shop/buy-mac/macbook-pro/13-inch) não cabe no meu bolso. :smile:
+Infelizmente o meu [Mac](https://www.apple.com/br/shop/buy-mac/macbook-pro/13-inch) já era. Atualmente serve a um propósito maior, é brinquedo da minha filha. :smile:
 
 ## Atenção :warning:
 Esta documentação funciona para mim, espero que funcione para você também. Use com sabedoria.
 
 ## Forma de uso :wrench:
-* Escolha o tipo de sistema.
-* Copie o checklist.
-* Crie uma nova issue com o checklist copiado.
+* Se for Debian, execute o script como root.
+* Se for Ubuntu, execute o script como usuário comum.
+* Se for Windows, siga o checklist.
 * Mãos a obra.
 * Boa sorte.
 
 ## Agradecimentos :tada:
-Este repositório foi inspirado nos trabalhos de [@mdo](https://github.com/mdo/config), [@holman](https://github.com/holman/dotfiles), [@jglovier](https://github.com/jglovier/my-setup).
+Este repositório foi inspirado nos trabalhos de [@mdo](https://github.com/mdo/config), [@holman](https://github.com/holman/dotfiles) e [@jglovier](https://github.com/jglovier/my-setup).
--- Workstation-Ubuntu/instalar.ubuntu.workstation.sh
+++ Workstation-Ubuntu/instalar.ubuntu.workstation.sh
@@ -2,7 +2,7 @@
 
 exibir_avisos_inicializacao() {
     echo "ATENCAO, ESTE SCRIPT E COMPATIVEL APENAS COM DISTRIBUICAO UBUNTU BIONIC."
-    echo "TESTADO E ATUALIZADO EM 13/05/2018."
+    echo "TESTADO E ATUALIZADO EM 26/06/2018."
     echo "USE POR SUA CONTA E RISCO."
 }
 
@@ -16,69 +16,84 @@ instalar() {
     instalar_sqlite3
     instalar_php
     instalar_virtualbox
+    instalar_latex
+    instalar_ruby
+    instalar_shhhhh
+    instalar_R
     exibir_avisos_encerramento
 }
 
 # Atualiza o Sistema Operacional para a versão mais recente possível.
 atualizar_so() {
-    apt-get update
-    apt-get upgrade
-    apt-get dist-upgrade
+    sudo apt-get update
+    sudo apt-get upgrade
+    sudo apt-get dist-upgrade
 }
 
 instalar_especificos() {
     # PLACA DE VIDEO NVIDIA GEFORCE GT740M
     #https://tecadmin.net/install-latest-nvidia-drivers-ubuntu/
-    add-apt-repository ppa:graphics-drivers/ppa
-    apt-get update
-    apt-get install nvidia-390
+    sudo add-apt-repository ppa:graphics-drivers/ppa
+    sudo apt-get update
+    sudo apt-get install -y nvidia-390
     #reboot
     #lsmod | grep nvidia
 }
 
 instalar_essenciais() {
-    apt-get install -y build-essential
+    # Ferramentas de compilação
+    sudo apt-get install -y build-essential
     # Editor de texto de linha de comando
-    apt-get install -y vim
+    sudo apt-get install -y vim
     # Ferramenta de descompressão
-    apt-get install -y unzip
+    sudo apt-get install -y unzip
     # Gerenciador de código-fonte
-    apt-get install -y git git-core
+    sudo apt-get install -y git git-core
     # Dependências do GnuCash
-    apt-get install -y gnupg2 dirmngr
+    sudo apt-get install -y gnupg2 dirmngr
     # Cliente de Torrent
-    apt-get install -y transmission
-    # Unrar
-    apt-get install -y unrar
+    sudo apt-get install -y transmission
+    # rar
+    sudo apt-get install -y unrar
     # CD/DVD Burner
-    apt-get install -y brasero
+    sudo apt-get install -y brasero
     # Audacity (edição de som)
-    add-apt-repository ppa:ubuntuhandbook1/audacity
-    apt-get update
-    apt-get install -y audacity
-    # 
+    sudo add-apt-repository ppa:ubuntuhandbook1/audacity
+    sudo apt-get update
+    sudo apt-get install -y audacity
     # ATOM.io
     cd /tmp
     wget https://github.com/atom/atom/releases/download/v1.26.1/atom-amd64.deb
-    apt install atom-amd64.deb
+    sudo apt install atom-amd64.deb
     rm atom-amd64.deb
     # NETBEANS
+    cd /tmp
     wget http://download.netbeans.org/netbeans/8.2/final/bundles/netbeans-8.2-linux.sh
-    chmod +x netbeans-8.2-linux.sh
-    bash netbeans-8.2-linux.sh
+    sudo chmod +x netbeans-8.2-linux.sh
+    sudo bash netbeans-8.2-linux.sh
     # JETBRAINS
-    apt-get install -y fuse
-    modprobe fuse
-    groupadd fuse
+    cd /tmp
+    sudo apt-get install -y fuse
+    sudo modprobe fuse
+    sudo groupadd fuse
     user="$(whoami)"
-    usermod -a -G fuse $user
+    sudo usermod -a -G fuse $user
     wget https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.8.3678.tar.gz
     tar -xvf jetbrains-toolbox-1.8.3678.tar.gz
     cd jetbrains-toolbox-1.8.3678
     ./jetbrains-toolbox
+    # Screen Recorder
+    sudo apt-get install -y kazam
+    # OBS Project
+    sudo apt-get install -y ffmpeg
+    sudo add-apt-repository ppa:obsproject/obs-studio && sudo apt-get update
+    sudo apt-get install -y obs-studio
+
 }
 
 instalar_java() {
+    # JAVA 8 (pendente)
+
     # JAVA 9
     # MAIS INFORMAÇÕES EM: http://www.webupd8.org/2015/02/install-oracle-java-9-in-ubuntu-linux.html
     #add-apt-repository ppa:webupd8team/java
@@ -87,20 +102,23 @@ instalar_java() {
 
     # JAVA 10
     # MAIS INFORMAÇÔES EM: https://www.linuxuprising.com/2018/04/install-oracle-java-10-in-ubuntu-or.html
-    add-apt-repository ppa:linuxuprising/java
-    apt-get update
-    apt-get install -y oracle-java10-installer
+    sudo add-apt-repository ppa:linuxuprising/java
+    sudo apt-get update
+    sudo apt-get install -y oracle-java10-installer
+
+    # Ferramentas
+    sudo apt-get install -y ant maven
 }
 
 instalar_postgresql() {
     # REPOSITÓRIO OFICIAL DO POSTGRESQL
     # MAIS INFORMAÇÕES EM: https://www.postgresql.org/download/linux/debian/
-    echo "deb http://apt.postgresql.org/pub/repos/apt/ bionic-pgdg main" | tee /etc/apt/sources.list.d/pgdg.list
-    wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
-    apt-get update
+    sudo echo "deb http://apt.postgresql.org/pub/repos/apt/ bionic-pgdg main" | sudo tee /etc/apt/sources.list.d/pgdg.list
+    wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+    sudo apt-get update
 
     # NA ORDEM: Servidor, modulos adicionais, bibliotecas e binarios cliente, desenvolvimento frontend, desenvolvimento backend, ferramenta administrativa
-    apt-get install -y postgresql-10 postgresql-contrib-10 postgresql-client-10 libpq-dev postgresql-server-dev-10
+    sudo apt-get install -y postgresql-10 postgresql-contrib-10 postgresql-client-10 libpq-dev postgresql-server-dev-10
 
     # ALTERACAO DA SENHA PADRAO
     su postgres
@@ -108,50 +126,50 @@ instalar_postgresql() {
     exit
 
     # Ferramentas de apoio
-    apt-get install -y pgadmin4
+    sudo apt-get install -y pgadmin4
 }
 
 instalar_sqlite3() {
-    apt-get install -y sqlite3
+    sudo apt-get install -y sqlite3
 }
 
 instalar_php() {
     # instala apache2 e php5
-    apt-get install -y apache2 php7.2 libapache2-mod-php7.2
+    sudo apt-get install -y apache2 php7.2 libapache2-mod-php7.2
     # instala extenção SQLite para php5
-    apt-get install -y php7.2-sqlite3
+    sudo apt-get install -y php7.2-sqlite3
     # instala extenção PostgreSQL para php5
-    apt-get install -y php7.2-pgsql
+    sudo apt-get install -y php7.2-pgsql
     # instala extenção GD (biblioteca gráfica) para php5
-    apt-get install -y php7.2-gd
+    sudo apt-get install -y php7.2-gd
     # instala extenção CURL
-    apt-get install -y php7.2-curl
+    sudo apt-get install -y php7.2-curl
     # instala extenção IMAP
-    apt-get install -y libc-client-dev php7.2-imap
-    phpenmod imap
+    sudo apt-get install -y libc-client-dev php7.2-imap
+    sudo phpenmod imap
     # instala extenção INTL (internationalization)
-    apt-get install -y php7.2-intl
+    sudo apt-get install -y php7.2-intl
     # extenção SOAP, JSON e XML
-    apt-get install -y php7.2-xml php7.2-soap php7.2-json php7.2-xmlrpc
+    sudo apt-get install -y php7.2-xml php7.2-soap php7.2-json php7.2-xmlrpc
     # extenção zip
-    apt-get install -y php7.2-zip
+    sudo apt-get install -y php7.2-zip
     # extenção mbstring
-    apt-get install -y php7.2-mbstring
+    sudo apt-get install -y php7.2-mbstring
     # extenção mcrypt
-    apt-get install -y php7.2-mcrypt
+    sudo apt-get install -y php7.2-mcrypt
     # extenção apcu para cache
-    apt-get install -y php7.2-apcu
+    sudo apt-get install -y php7.2-apcu
     # extenção MySQL/MariaDB
-    apt-get install -y php7.2-mysql
+    sudo apt-get install -y php7.2-mysql
 }
 
 instalar_virtualbox() {
-    echo "deb http://download.virtualbox.org/virtualbox/debian bionic contrib" | tee /etc/apt/sources.list.d/virtualbox.list
-    wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | apt-key add -
-    wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | apt-key add -
-    apt-get update
+    sudo echo "deb http://download.virtualbox.org/virtualbox/debian bionic contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
+    wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+    wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+    sudo apt-get update
     # Note: Ubuntu/Debian users might want to install the dkms package to ensure that the VirtualBox host kernel modules (vboxdrv, vboxnetflt and vboxnetadp) are properly updated if the linux kernel version changes during the next apt-get upgrade
-    apt-get install -y virtualbox-5.2 dkms
+    sudo apt-get install -y virtualbox-5.2 dkms
 
     # VirtualBox Extension Pack
     version=$(vboxmanage -v)
@@ -167,9 +185,9 @@ instalar_virtualbox() {
 }
 
 instalar_latex() {
-    apt-get install -y texlive texlive-lang-portuguese texlive-lang-english texlive-fonts-recommended texlive-extra-utils texlive-generic-extra texlive-plain-extra texlive-publishers texlive-latex-recommended texlive-science texlive-bibtex-extra texlive-font-utils texlive-fonts-extra texlive-fonts-extra-links texlive-fonts-recommended texlive-latex-extra texlive-pictures
+    sudo apt-get install -y texlive texlive-lang-portuguese texlive-lang-english texlive-fonts-recommended texlive-extra-utils texlive-generic-extra texlive-plain-extra texlive-publishers texlive-latex-recommended texlive-science texlive-bibtex-extra texlive-font-utils texlive-fonts-extra texlive-fonts-extra-links texlive-fonts-recommended texlive-latex-extra texlive-pictures
     #EDITORES
-    apt-get install -y texmaker
+    sudo apt-get install -y texmaker
     #apt-get install -y texstudio
 }
 
@@ -178,14 +196,18 @@ instalar_ruby() {
     sudo apt-get install -y ruby ruby-dev rubygems libruby ruby-full
     sudo apt-get install -y nodejs
     sudo gem install rails
-    sudo gem install jekyll
     sudo gem install bundler
+    sudo gem install jekyll
 }
 
 # :^)
 instalar_shhhhh() {
+    #n64
     sudo apt-get install -y libmupen64plus-dev libmupen64plus2 libretro-mupen64plus mupen64plus-audio-all mupen64plus-audio-sdl mupen64plus-data mupen64plus-input-all mupen64plus-input-sdl mupen64plus-qt mupen64plus-rsp-all mupen64plus-rsp-hle mupen64plus-rsp-z64 mupen64plus-ui-console mupen64plus-video-all mupen64plus-video-arachnoid mupen64plus-video-glide64 mupen64plus-video-glide64mk2 mupen64plus-video-rice mupen64plus-video-z64
+}
 
+instalar_R() {
+    sudo apt-get install -y r-base r-base-dev
 }
 
 exibir_avisos_encerramento() {
@@ -194,9 +216,6 @@ exibir_avisos_encerramento() {
     echo "http://ubuntuserverguide.com/2012/10/how-to-enable-and-configure-apache2-userdir-module-in-ubuntu-server-12-04.html"
     echo "2. Crie uma chave SSH"
     echo "ssh-keygen -t rsa -b 4096 -C \"your_email@example.com\""
-    echo "3. Complementares"
-    echo "3.1. JetBrains"
-    echo "3.2. NetBeans"
 }
 
 # INICIAR PROCESSO DE INSTALAÇÃO
