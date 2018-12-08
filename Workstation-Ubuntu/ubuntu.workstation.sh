4b1b6e97fdeeb518d221eccd9d8371ec30e96cfc
Guilherme de Lemos <guilherme.eti@gmail.com>
2018-12-08 14:05:23 -0200

    Ubuntu 18.04

--- Workstation-Ubuntu/ubuntu.workstation.sh
+++ Workstation-Ubuntu/ubuntu.workstation.sh
@@ -8,18 +8,44 @@ criar_diretorios() {
     pwd
     mkdir Projetos
     mkdir public_html
+    mkdir Ferramentas
+}
+
+upgrade_so() {
+    echo "=============================="
+    echo "| UPGRADE DOS PACOTES/SO     |"
+    echo "=============================="
+    sudo apt update
+    sudo apt upgrade
 }
 
 instalar_essenciais() {
     echo "=============================="
     echo "| ESSENCIAIS                 |"
     echo "=============================="
-
-    sudo apt update
-    sudo apt install -y vim curl
-
+    # Editor de texto de linha de comando
+    sudo apt install -y vim
+    # Transferência de dados usando URLs
+    sudo apt install -y curl
+    # Gerenciadorer de código-fonte
     sudo apt install -y git
-    configurar_git
+    # Ferramentas de compilação
+    sudo apt install -y build-essential
+    # Ferramenta de descompressão
+    sudo apt install -y unzip unrar
+    # Cliente de Torrent
+    sudo apt install -y transmission
+    # CD/DVD Burner
+    sudo apt install -y brasero
+}
+
+instalar_audacity() {
+    echo "====================================="
+    echo "| INSTALAR AUDACITY (editor de som) |"
+    echo "====================================="
+    sudo add-apt-repository ppa:ubuntuhandbook1/audacity
+    sudo apt update
+    sudo apt install -y audacity
 }
 
 instalar_sdkman() {
@@ -42,9 +68,188 @@ instalar_linguagens() {
     sdk install gradle
     sdk install grails
     sdk install maven
-    # php ruby python :'(
+    echo "php ruby python :'("
 }
 
+instalar_sqlite() {
+    echo "=============================="
+    echo "| INSTALAÇÃO SQLITE3         |"
+    echo "=============================="
+    sudo apt install -y sqlite3
+}
+
+instalar_latex() {
+    echo "=============================="
+    echo "| INSTALAÇÃO LATEX            |"
+    echo "=============================="
+    sudo apt install -y texlive texlive-lang-portuguese texlive-lang-english texlive-fonts-recommended texlive-extra-utils texlive-generic-extra texlive-plain-extra texlive-publishers texlive-latex-recommended texlive-science texlive-bibtex-extra texlive-font-utils texlive-fonts-extra texlive-fonts-extra-links texlive-fonts-recommended texlive-latex-extra texlive-pictures
+    #EDITORES
+    sudo apt install -y texmaker
+    #apt install -y texstudio
+}
+
+instalar_atom() {
+    echo "=============================="
+    echo "| INSTALAÇÃO ATOM.io         |"
+    echo "=============================="
+    cd /tmp
+    wget https://atom.io/download/deb -O atom-amd64.deb
+    sudo apt install ./atom-amd64.deb
+    rm atom-amd64.deb
+}
+
+instalar_netbeans() {
+    echo "=============================="
+    echo "| INSTALAÇÃO NETBEANS        |"
+    echo "=============================="
+    cd ~/Ferramentas
+    wget http://ftp.unicamp.br/pub/apache/incubator/netbeans/incubating-netbeans-java/incubating-9.0/incubating-netbeans-java-9.0-bin.zip
+    unzip incubating-netbeans-java-9.0-bin.zip
+    rm incubating-netbeans-java-9.0-bin.zip
+}
+
+instalar_postgresql() {
+    echo "=============================="
+    echo "| INSTALAÇÃO POSTGRESQL      |"
+    echo "=============================="
+    # REPOSITÓRIO OFICIAL DO POSTGRESQL
+    # MAIS INFORMAÇÕES EM: https://www.postgresql.org/download/linux/debian/
+    sudo echo "deb http://apt.postgresql.org/pub/repos/apt/ bionic-pgdg main" | sudo tee /etc/apt/sources.list.d/pgdg.list
+    wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+    sudo apt update
+
+    # NA ORDEM: Servidor, modulos adicionais, bibliotecas e binarios cliente, desenvolvimento frontend, desenvolvimento backend, ferramenta administrativa
+    sudo apt install -y postgresql-10 postgresql-contrib-10 postgresql-client-10 libpq-dev postgresql-server-dev-10
+
+    # TODO Alterar senha padrão do POSTGRESQL
+    # ALTERACAO DA SENHA PADRAO
+    #sudo su postgres
+    #psql -c "ALTER USER postgres WITH ENCRYPTED PASSWORD 'postgres';"
+    #exit
+
+    # Ferramentas de apoio
+    sudo apt install -y pgadmin4
+}
+instalar_intellij() {
+    echo "=============================="
+    echo "| INSTALAÇÃO INTELLIJ        |"
+    echo "=============================="
+    cd /tmp
+    sudo apt install -y fuse
+    sudo modprobe fuse
+    sudo groupadd fuse
+    user="$(whoami)"
+    sudo usermod -a -G fuse $user
+    wget https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.12.4481.tar.gz
+    tar -xvf jetbrains-toolbox-1.12.4481.tar.gz
+    cd jetbrains-toolbox-1.12.4481
+    ./jetbrains-toolbox
+}
+instalar_obsstudio() {
+    echo "=============================="
+    echo "| INSTALAÇÃO OBS STUDIO      |"
+    echo "=============================="
+    sudo apt install -y ffmpeg
+    sudo add-apt-repository ppa:obsproject/obs-studio && sudo apt update
+    sudo apt install -y obs-studio
+}
+instalar_gnucash() {
+    echo "=============================="
+    echo "| INSTALAÇÃO GNUCASH         |"
+    echo "=============================="
+    sudo apt install -y gnupg2 dirmngr
+    sudo apt install -y gnucash
+}
+
+instalar_youtubedl() {
+  echo "=============================="
+  echo "| INSTALAÇÃO YOUTUBE DL      |"
+  echo "=============================="
+  echo http://www.webupd8.org/2017/08/youtube-dl-gui-04-ships-with-new-user.html
+  echo https://github.com/MrS0m30n3/youtube-dl-gui
+
+  sudo add-apt-repository ppa:nilarimogard/webupd8
+  sudo apt install -y youtube-dlg
+}
+
+instalar_virtualbox() {
+    echo "=============================="
+    echo "| INSTALAÇÃO VIRTUAL BOX     |"
+    echo "=============================="
+    sudo echo "deb http://download.virtualbox.org/virtualbox/debian bionic contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
+    wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+    wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+    sudo apt-get update
+    # Note: Ubuntu/Debian users might want to install the dkms package to ensure that the VirtualBox host kernel modules (vboxdrv, vboxnetflt and vboxnetadp) are properly updated if the linux kernel version changes during the next apt-get upgrade
+    sudo apt-get install -y virtualbox-5.2 dkms
+
+    # VirtualBox Extension Pack
+    version=$(vboxmanage -v)
+    #echo $version
+    var1=$(echo $version | cut -d 'r' -f 1)
+    #echo $var1
+    var2=$(echo $version | cut -d 'r' -f 2)
+    #echo $file
+    #echo $var2
+    file="Oracle_VM_VirtualBox_Extension_Pack-$var1-$var2.vbox-extpack"
+    wget http://download.virtualbox.org/virtualbox/$var1/$file -O /tmp/$file
+    VBoxManage extpack install /tmp/$file --replace
+}
+instalar_r() {
+    echo "=============================="
+    echo "| INSTALAÇÃO R               |"
+    echo "=============================="
+    sudo apt install -y r-base r-base-dev
+}
+instalar_android() {
+    # TODO Android/Emuladores
+    echo "=============================="
+    echo "| INSTALAÇÃO ANDROID         |"
+    echo "=============================="
+    echo "TODO :/"
+}
+instalar_php() {
+    # TODO PHP
+    echo "=============================="
+    echo "| INSTALAÇÃO PHP             |"
+    echo "=============================="
+    echo "TODO :/"
+}
+instalar_ruby() {
+    # TODO Ruby
+    echo "=============================="
+    echo "| INSTALAÇÃO RUBY            |"
+    echo "=============================="
+    echo "TODO :/"
+}
+instalar_python() {
+    # TODO Python
+    echo "=============================="
+    echo "| INSTALAÇÃO PYTHON          |"
+    echo "=============================="
+    echo "TODO :/"
+}
+instalar_nvidia() {
+    # TODO Placa de Vídeo
+    echo "=============================="
+    echo "| INSTALAÇÃO NVIDIA          |"
+    echo "=============================="
+    echo "TODO :/"
+    # PLACA DE VIDEO NVIDIA GEFORCE GT740M
+    echo https://tecadmin.net/install-latest-nvidia-drivers-ubuntu/
+    sudo add-apt-repository ppa:graphics-drivers/ppa
+    sudo apt-get update
+    sudo apt-get install -y nvidia-390
+    lsmod | grep nvidia
+    #reboot
+}
+instalar_heroku() {
+    echo "=============================="
+    echo "| INSTALAÇÃO HEROKU          |"
+    echo "=============================="
+    sudo snap install heroku --classic
+    heroku login
+}
 gerar_SSHKey() {
     echo "=============================="
     echo "| GERAÇÃO DA CHAVE SSH       |"
@@ -66,6 +271,9 @@ gerar_GPGKey() {
 }
 
 configurar_git() {
+    echo "=============================="
+    echo "| CONFIGURAÇÃO DO GIT        |"
+    echo "=============================="
     read -p "GIT user.name: " GIT_NAME
     read -p "GIT user.email: " GIT_EMAIL
     git config --global user.email "$GIT_EMAIL"
@@ -121,12 +329,42 @@ instalar() {
     echo "| INÍCIO DA INSTALAÇÃO       |"
     echo "=============================="
     criar_diretorios
+    upgrade_so
     instalar_essenciais
+
     instalar_sdkman
     instalar_linguagens
+    instalar_r
+    instalar_latex
+    instalar_php    # TODO PHP
+    instalar_ruby    # TODO Ruby
+    instalar_python  # TODO Python
+
+    instalar_audacity
+    instalar_obsstudio
+    instalar_youtubedl
+    instalar_gnucash
+    instalar_virtualbox
+    instalar_android # TODO Android
+
+    instalar_sqlite
+    instalar_postgresql
+
+    instalar_atom
+    instalar_netbeans
+    instalar_intellij
+
+    instalar_heroku
+
     gerar_SSHKey
     gerar_GPGKey
     configurar_github
+    configurar_git
+
+    instalar_nvidia
+    echo "=============================="
+    echo "| FIM DA INSTALAÇÃO          |"
+    echo "=============================="
 }
 
 instalar
